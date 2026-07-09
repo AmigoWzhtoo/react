62b630774c51a8a61f3aa1eac5851d5086122037
AmigoWzhtoo <wzhtooamigogamecenter@gmail.com>
2026-07-09 19:03:40 +0630

    Section_199

--- 10-react-quiz/src/components/App.js
+++ 10-react-quiz/src/components/App.js
@@ -48,6 +48,16 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return { ...initialState, questions: state.questions, status: "ready" };
+    // return {
+    //   ...state,
+    //   points: 0,
+    //   highscore: 0,
+    //   index: 0,
+    //   answer: null,
+    //   state: "ready",
+    // };
 
     default:
       throw new Error("Action unknown");
@@ -108,6 +118,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxProssiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
