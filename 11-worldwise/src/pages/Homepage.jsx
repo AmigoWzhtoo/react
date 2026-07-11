f1dea410500be87ab4dc3178dffea625826dce15
AmigoWzhtoo <wzhtooamigogamecenter@gmail.com>
2026-07-11 11:37:44 +0630

    11-worldwise

---
+++ 11-worldwise/src/pages/Homepage.jsx
@@ -0,0 +1,9 @@
+function Homepage() {
+  return (
+    <div>
+      <h3>WorldWise</h3>
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ 11-worldwise/src/pages/PageNotFound.jsx
@@ -0,0 +1,9 @@
+function PageNotFound() {
+  return (
+    <div>
+      <p>Page Not found</p>
+    </div>
+  );
+}
+
+export default PageNotFound;
